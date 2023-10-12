163bd6bd9eff46ea639829e8c28598db759f0c4d
aditigoyal291 <goyaladiti2912@gmail.com>
2023-10-12 23:52:12 +0530

    Added a new file in hashmap

--- BinaryTree/morristraversal.cpp
+++ BinaryTree/morristraversal.cpp
@@ -1,24 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-//tc:o(n) sc:o(1)
-//whenever in ques it is given that we have to use space complexity o(1) in case of a tree use morris traversal
-class Node{
-    public:
+// tc:o(n) sc:o(1)
+// whenever in ques it is given that we have to use space complexity o(1) in case of a tree always use morris traversal
+class Node
+{
+public:
     int data;
-    Node* right;
-    Node* left;
+    Node *right;
+    Node *left;
 
     Node(int data)
     {
-        this->data=data;
-        this->left=NULL;
-        this->right=NULL;
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
 
-
 int main()
 {
-    
+
     return 0;
 }
